9c38054c92b02561cd6d7c8eb0cf1b7701f65ae7
rodrimadrid <rodri.fm94@gmail.com>
2021-04-16 23:46:09 -0300

    sabores

---
+++ js/main.js
@@ -0,0 +1,51 @@
+class PRODUCTO {
+  constructor(bebidas) {
+    this.id = bebidas.id;
+    this.bebida = bebidas.bebida;
+    this.nombre = bebidas.nombre;
+    this.marca = bebidas.marca;
+    this.ml = bebidas.ml;
+    this.precioUnitario = bebidas.precioUnitario;
+    this.precioCaja = bebidas.precioCaja * 6;
+    this.img = bebidas.img;
+    this.vendido = false;
+    this.cantidadUnitaria = 1;
+    this.cantidadCaja = 1;
+  }
+  addCaja() {
+    this.cantidadCaja ++;
+  }
+  addUnitaria() {
+    this.cantidadUnitaria ++;
+  }
+  getTotalU() {
+    return (this.cantidadUnitaria * this.precioUnitario)
+  }
+  getTotalC() {
+    return (this.cantidadCaja * this.precioCaja)
+  }
+
+}
+// eventos load y ready
+window.addEventListener('load', () => {
+  $('.header').html(` <button class="btnCarrito openbtn float-right" >
+                        <img id='icon' src="images/shopping-cart.svg" >
+                      </button>`)
+  $('.header').append(` <div class="animacion">
+                        </div>`)
+
+  //feather.replace()
+  carritoToggle();
+  guardarCarrito();
+})
+$(document).ready(() => {
+  spinner();
+  limpiarCarrito();
+  // muestro productos con AJAX
+  $.getJSON("data/productos.json", function(datos, status){
+    mostrarProductos(datos);
+    eventosForm(datos);
+
+  })
+
+})
