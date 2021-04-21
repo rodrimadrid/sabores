b11477bcac4a9d24ec391786922da436e9d52f7c
rodrimadrid <rodri.fm94@gmail.com>
2021-04-21 00:31:28 -0300

    finished

--- js/components.js
+++ js/components.js
@@ -1,5 +1,6 @@
 let seleccionUnitario = [];
 let seleccionCaja = [];
+
 //provincias API
 const APIPROVINCIAS = "https://apis.datos.gob.ar/georef/api/provincias"
 //funcion para mostrar productos
@@ -15,37 +16,23 @@ function mostrarProductos(listado) {
 //funcion para mostrar productos seleccionados
 function mostrarSeleccionU() {
   $('#carrito').empty();
-
   for (var dato of seleccionUnitario) {
     $("#carrito").append(`<div id='producto${dato.id}' class='carrito__producto'>
                             <h4>${dato.nombre}</h4>
                             <div class="producto__tabla">
-                              <img style='height: 100px; width: 60px'src="${dato.img}" alt="product image">
+                              <img class='carrito-img' src="${dato.img}" alt="product image">
                               <h6  class="carrito-h6">Cantidad: ${dato.cantidadUnitaria}<br><br>precio: $${parseInt((dato.cantidadUnitaria * dato.precioUnitario).toFixed(2))} </h6>
-                              <a href="javascript:void(0)" id='${dato.id}' class='delete'>&times;</a>
+                              <div class='botones'>
+                                <button class="btn btn-success plus"type="button" id='${dato.id}'>+</button>
+                                <button class="btn btn-danger delete"type="button" id='${dato.id}'>-</button>
+                              </div>
                              </div>
                           </div>`)
   }
   localStorage.setItem('carritoU', JSON.stringify(seleccionUnitario));
 
-
-  $('.delete').click((e) => { //evento para eliminar producto del carrito
-
-    var removeU = seleccionUnitario.find(producto => producto.id == e.target.id);
-    index = removeU.id
-    if (removeU.cantidadUnitaria > 1) {
-      removeU.cantidadUnitaria--;
-      mostrarSeleccionU();
-      precioFinal()
-    } else {
-      seleccionUnitario = seleccionUnitario.filter(function(i) {
-        return i !== removeU
-      });
-      console.log(seleccionUnitario)
-      $(`#producto${index}`).remove();
-      precioFinal()
-    }
-  })
+  sumarU()
+  eliminarU()
   precioFinal();
 }
 
@@ -55,31 +42,18 @@ function mostrarSeleccionC() {
     $("#carrito2").append(`<div id='producto${dato.id}' class='carrito__producto'>
                             <h4>${dato.nombre}</h4>
                             <div class="producto__tabla">
-                              <img style='height: 100px; width: 60px'src="${dato.img}" alt="product image">
+                              <img class='carrito-img'src="${dato.img}" alt="product image">
                               <h6  class="carrito-h6">Cantidad: ${dato.cantidadCaja}<br><br>precio: $${parseInt((dato.cantidadCaja * dato.precioCaja).toFixed(2))} </h6>
-                              <a href="javascript:void(0)" id='${dato.id}' class='delete'>&times;</a>
+                              <div class='botones'>
+                                <button class="btn btn-success plus"type="button" id='${dato.id}'>+</button>
+                                <button class="btn btn-danger delete"type="button" id='${dato.id}'>-</button>
+                              </div>
                              </div>
                           </div>`)
   }
   localStorage.setItem('carritoC', JSON.stringify(seleccionCaja));
-
-  $('.delete').click((e) => { //evento para eliminar producto del carrito
-    e.preventDefault();
-    var removeC = seleccionCaja.find(producto => producto.id == e.target.id);
-    index = removeC.id
-    if (removeC.cantidadCaja > 1) {
-      removeC.cantidadCaja--;
-      mostrarSeleccionC();
-      precioFinal()
-    } else {
-      seleccionCaja = seleccionCaja.filter(function(i) {
-        return i !== removeC
-      });
-      console.log(seleccionCaja)
-      $(`#producto${index}`).remove();
-      precioFinal()
-    }
-  })
+  sumarC()
+  eliminarC()
   precioFinal()
 }
 //funcion para crear productos
@@ -97,80 +71,9 @@ function crearProducto(dato) {
                           </div>`
   )
 }
-//Funcion para seleccionar productos por unidad
-function btnUnidad(listado) {
-  return $(".btn-unidad").click((e) => {
-    let producto = seleccionUnitario.find(producto => producto.id == e.target.id);
-    let seleccionado = listado.find(producto => producto.id == e.target.id);
-    if (producto != undefined) {
-      producto.cantidadUnitaria++;
-    } else {
 
-      let newProducto = new PRODUCTO(seleccionado);
-      seleccionUnitario.push(newProducto);
-      newProducto.cantidadCaja = 0;
-    }
-    $('.animacion').empty();
-    mostrarSeleccionU();
-    console.log(seleccionUnitario)
 
-    $('.animacion').append(`
-                              <img style='height: 120px; width: 100px'src="${seleccionado.img}" alt="product image">
-                                <div class="producto__notif">
-                                 <h6>${seleccionado.nombre}</h6><br>
-                                 <p>precio: $${seleccionado.precioUnitario}<p>
-                                </div>`)
-
-    $('.animacion').animate({
-        right: '15px'
-      }, "slow")
-      .delay(1500)
-      .animate({
-        right: '-45%'
-      }, "slow")
-  })
-}
-//funcion para seleccionar productos por caja
-function btnCaja(listado) {
-  return $(".btn-caja").click((e) => {
-    let producto = seleccionCaja.find(producto => producto.id == e.target.id);
-    let seleccionado = listado.find(producto => producto.id == e.target.id);
-    if (producto != undefined) {
-      producto.cantidadCaja++;
-    } else {
-      let newProducto = new PRODUCTO(seleccionado);
-      seleccionCaja.push(newProducto);
-      newProducto.cantidadUnitaria = 0;
-    }
-    $('.animacion').empty();
-    mostrarSeleccionC();
-    console.log(seleccionCaja)
-    $('.animacion').append(`
-                              <img style='height: 120px; width: 100px'src="${seleccionado.img}" alt="product image">
-                                <div class="producto__notif">
-                                 <h6>${seleccionado.nombre}</h6><br>
-                                 <p>precio: $${seleccionado.precioCaja * 6} <p>
-                                </div>`)
-    $('.animacion').animate({
-        right: '15px'
-      }, "slow")
-      .delay(1500)
-      .animate({
-        right: '-45%'
-      }, "slow")
-  })
-}
-//boton para resetear carritos
-function limpiarCarrito() {
-  return $(".cleanbtn").click((e) => {
-    $("div.carrito").empty();
-    $(".precio-total").empty()
-    seleccionUnitario = [];
-    seleccionCaja = [];
-    localStorage.clear()
-  })
-}
-// boton para realizar compra
+// boton para realizar pedido
 function comprar(){
   $('#btnCompra').click((e)=>{
     $('#contenedor').empty()
@@ -247,35 +150,6 @@ function comprar(){
       }
 })
 }
-//suma total de compra
-function precioFinal() {
-
-  $(".precio-total").empty()
-  $(".precio-total").append(`
-                                <span>Precio total: $${(sumaFinalU() + sumaFinalC()).toFixed(2)} </span>
-                              `)
-
-
-
-}
-
-function sumaFinalU() {
-  let precioFinalU = 0
-  for (var precio of seleccionUnitario) {
-    let precioTotal = precio.cantidadUnitaria * precio.precioUnitario
-    precioFinalU += precioTotal
-  }
-  return precioFinalU
-}
-
-function sumaFinalC() {
-  let precioFinalC = 0
-  for (var precio of seleccionCaja) {
-    let precioTotal = precio.cantidadCaja * precio.precioCaja
-    precioFinalC += precioTotal
-  }
-  return precioFinalC
-}
 $('buybtn').click((e) => {
   sumaFinal()
 })
@@ -287,8 +161,8 @@ function spinner() {
 }
 // sidebar // CARRITO
 function openNav() {
-  document.getElementById("mySidebar").style.width = "300px";
-  document.getElementById("main").style.marginLeft = "300px";
+  document.getElementById("mySidebar").style.width = "325px";
+  document.getElementById("main").style.marginLeft = "325px";
 }
 
 function closeNav() {
---
+++ js/function.js
@@ -0,0 +1,156 @@
+//sumar prpoducto
+function sumarU(){
+$('.plus').click((e)=>{
+  let sumar = seleccionUnitario.find(producto => producto.id == e.target.id);
+  sumar.cantidadUnitaria ++;
+  mostrarSeleccionU();
+  precioFinal()
+})
+}
+//sumar prpoducto
+function sumarC(){
+$('.plus').click((e)=>{
+  let sumar = seleccionCaja.find(producto => producto.id == e.target.id);
+  sumar.cantidadCaja ++;
+  mostrarSeleccionC();
+  precioFinal()
+})
+}
+//eliminar producto del carrito
+function eliminarU(){
+  $('.delete').click((e) => {
+    console.log(e.target.id)
+    var removeU = seleccionUnitario.find(producto => producto.id == e.target.id);
+    const indexU = seleccionUnitario.indexOf(removeU)
+    console.log(indexU)
+    if (removeU.cantidadUnitaria > 1) {
+      removeU.cantidadUnitaria--;
+      mostrarSeleccionU();
+      precioFinal()
+    } else {
+      seleccionUnitario = seleccionUnitario.filter( (i)=> (i !== removeU) );
+      mostrarSeleccionU();
+      precioFinal()
+    }
+  })
+}
+//evento para eliminar producto del carrito2
+function eliminarC(){
+  $('.delete').click((e) => {
+    e.preventDefault();
+    var removeC = seleccionCaja.find(producto => producto.id == e.target.id);
+    let indexC = seleccionCaja.indexOf(removeC);
+    console.log(indexC);
+    if (removeC.cantidadCaja == 1) {
+      seleccionCaja = seleccionCaja.filter( (i)=> (i !== removeC) );
+      console.log(seleccionCaja)
+      mostrarSeleccionC();
+      precioFinal()
+    } else {
+      removeC.cantidadCaja--;
+      mostrarSeleccionC();
+      precioFinal()
+    }
+  })
+}
+//boton para resetear carritos
+function limpiarCarrito() {
+  return $(".cleanbtn").click((e) => {
+    $("div.carrito").empty();
+    $(".precio-total").empty()
+    seleccionUnitario = [];
+    seleccionCaja = [];
+    localStorage.clear()
+  })
+}
+//Funcion para seleccionar productos por unidad
+function btnUnidad(listado) {
+  return $(".btn-unidad").click((e) => {
+    let producto = seleccionUnitario.find(producto => producto.id == e.target.id);
+    let seleccionado = listado.find(producto => producto.id == e.target.id);
+    if (producto != undefined) {
+      producto.cantidadUnitaria++;
+    } else {
+      let newProducto = new PRODUCTO(seleccionado);
+      seleccionUnitario.push(newProducto);
+      newProducto.cantidadCaja = 0;
+    }
+    $('.animacion').empty();
+    mostrarSeleccionU();
+    console.log(seleccionUnitario)
+
+    $('.animacion').append(`
+                              <img style='height: 120px; width: 100px'src="${seleccionado.img}" alt="product image">
+                                <div class="producto__notif">
+                                 <h6>${seleccionado.nombre}</h6><br>
+                                 <p>precio: $${seleccionado.precioUnitario}<p>
+                                </div>`)
+
+    $('.animacion').animate({
+        right: '15px'
+      }, "slow")
+      .delay(1500)
+      .animate({
+        right: '-45%'
+      }, "slow")
+  })
+}
+//funcion para seleccionar productos por caja
+function btnCaja(listado) {
+  return $(".btn-caja").click((e) => {
+    let producto = seleccionCaja.find(producto => producto.id == e.target.id);
+    let seleccionado = listado.find(producto => producto.id == e.target.id);
+    if (producto != undefined) {
+      producto.cantidadCaja++;
+    } else {
+      let newProducto = new PRODUCTO(seleccionado);
+      seleccionCaja.push(newProducto);
+      newProducto.cantidadUnitaria = 0;
+    }
+    $('.animacion').empty();
+    mostrarSeleccionC();
+    console.log(seleccionCaja)
+    $('.animacion').append(`
+                              <img style='height: 120px; width: 100px'src="${seleccionado.img}" alt="product image">
+                                <div class="producto__notif">
+                                 <h6>${seleccionado.nombre}</h6><br>
+                                 <p>precio: $${seleccionado.precioCaja * 6} <p>
+                                </div>`)
+    $('.animacion').animate({
+        right: '15px'
+      }, "slow")
+      .delay(1500)
+      .animate({
+        right: '-45%'
+      }, "slow")
+  })
+}
+//suma total de compra
+function precioFinal() {
+
+  $(".precio-total").empty()
+  $(".precio-total").append(`
+                                <span>Precio total: $${(sumaFinalU() + sumaFinalC()).toFixed(2)} </span>
+                              `)
+
+
+
+}
+
+function sumaFinalU() {
+  let precioFinalU = 0
+  for (var precio of seleccionUnitario) {
+    let precioTotal = precio.cantidadUnitaria * precio.precioUnitario
+    precioFinalU += precioTotal
+  }
+  return precioFinalU
+}
+
+function sumaFinalC() {
+  let precioFinalC = 0
+  for (var precio of seleccionCaja) {
+    let precioTotal = precio.cantidadCaja * precio.precioCaja
+    precioFinalC += precioTotal
+  }
+  return precioFinalC
+}
